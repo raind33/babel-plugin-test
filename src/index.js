94ffc41124d0b1afb8dc8c8029763d6e66253d8e
wuyl <wuyl@yxt.com>
2020-05-22 09:10:39 +0800

    test:test cherry-picking

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-const t = require('@babel/types')
+const explode = require("@babel/helper-explode-assignable-expression").default;
+const t = require("@babel/types");
 
+const operator = '**';
 const getMathPowExpression = (left, right) => {
   return t.callExpression(
     t.memberExpression(t.identifier("Math"), t.identifier("pow")),
@@ -7,18 +9,35 @@ const getMathPowExpression = (left, right) => {
   );
 }
 
-
-module.exports = function () {
+module.exports = function() {
   return {
-    name: 'babel-plugin-exponentiation-operatorrain',
+    name: 'babel-plugin-exponentiation-operator',
     visitor: {
-      BinaryExpression (path) {
-        console.log(path)
-        const { node  } = path
-        if (node.operator === '**') {
+      AssignmentExpression(path) {
+        const { node, scope } = path;
+        // 只处理 **=
+        if (node.operator === `${operator}=`) {
+          const nodes = [];
+          // 找一个不会炸掉的变量名
+          const exploded = explode(node.left, nodes, this, scope);
+          nodes.push(
+            t.assignmentExpression(
+              "=",
+              exploded.ref,
+              getMathPowExpression(exploded.uid, node.right),
+            ),
+          );
+          console.log(JSON.parse(JSON.stringify(nodes)))
+          path.replaceWithMultiple(nodes);
+        }
+      },
+
+      BinaryExpression(path) {
+        const { node } = path;
+        if (node.operator === operator) {
           path.replaceWith(getMathPowExpression(node.left, node.right));
         }
-      }
+      },
     }
-  }
+  };
 }
--- src/test/index2.js
+++ src/test/index2.js
@@ -1,1 +1,14 @@
+var _a, _a2, _a3, _a4;
+
 Math.pow(a, b);
+_a = a;
+a = Math.pow(_a, b);
+Math.pow(a, b);
+_a2 = a;
+a = Math.pow(_a2, b);
+Math.pow(a, b);
+_a3 = a;
+a = Math.pow(_a3, b);
+Math.pow(a, b);
+_a4 = a;
+a = Math.pow(_a4, b);
